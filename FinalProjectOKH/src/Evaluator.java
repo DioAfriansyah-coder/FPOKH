d5f7fef95a77847b7d6b63efba456a1d3d034309
DioAfriansyah-coder <61171149+DioAfriansyah-coder@users.noreply.github.com>
2020-04-27 16:57:49 +0700

    Add files via upload

---
+++ FinalProjectOKH/src/Evaluator.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Or
+ */
+public class Evaluator {
+    public static double getPenalty(int[][] matrix, int[][] jadwal, int jumlahMurid) {
+            double penalty = 0;
+		for(int i = 0; i < matrix.length - 1; i++) {
+                    for(int j = i+1; j < matrix.length; j++) {
+                        if(matrix[i][j] != 0) {
+                            if(Math.abs(jadwal[j][1] - jadwal[i][1]) >= 1 && Math.abs(jadwal[j][1] - jadwal[i][1]) <= 5) {
+				penalty = penalty + (matrix[i][j] * (Math.pow(2, 5-(Math.abs(jadwal[j][1] - jadwal[i][1])))));
+		}
+            }
+	}
+    }return penalty/jumlahMurid;
+}
+}
